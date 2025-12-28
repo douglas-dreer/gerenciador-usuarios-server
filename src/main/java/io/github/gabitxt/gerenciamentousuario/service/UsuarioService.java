b1efae1daf7f02a587fde23dc010543c8cee9474
Douglas Dreer <douglasdreer@outlook.com.br>
2025-12-27 21:54:38 -0300

    add user management methods to UsuarioService interface

--- src/main/java/io/github/gabitxt/gerenciamentousuario/service/UsuarioService.java
+++ src/main/java/io/github/gabitxt/gerenciamentousuario/service/UsuarioService.java
@@ -5,9 +5,39 @@ import io.github.gabitxt.gerenciamentousuario.model.UsuarioDTO;
 import java.util.List;
 
 public interface UsuarioService {
+    /**
+     * Cria um novo usuário.
+     * @param usuarioDTO
+     * @return UsuarioDTO criado
+     */
     UsuarioDTO criarUsuario(UsuarioDTO usuarioDTO);
+
+    /**
+     * Lista todos os usuários.
+     * @return lista de UsuarioDTO
+     */
     List<UsuarioDTO> listarUsuarios();
+
+    /**
+     * Busca um usuário por ID.
+     * @param id
+     * @return {@UsuarioDTO} ou null
+     */
     UsuarioDTO buscarUsuarioPorId(Long id);
+
+    /**
+     * Atualiza um usuário existente.
+     * @param id
+     * @param usuarioDTO
+     * @return
+     * @throws Exception
+     */
     UsuarioDTO atualizarUsuario(Long id, UsuarioDTO usuarioDTO) throws Exception;
+
+    /**
+     * Deleta um usuário por ID.
+     * @param id
+     * @throws Exception
+     */
     void deletarUsuario(Long id) throws Exception;
 }
